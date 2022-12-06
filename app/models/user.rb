bb38396d14900606f7ad4f846f4055f12c5288e8
EC2 Default User <yotchan32323@gmail.com>
2022-12-06 05:09:50 +0000

    ユーザー編集画面

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 100 }
 
   def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpeg'
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpeg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(gravity: :center, resize: "150x150", crop: "150x150+0+0").processed
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   resources :posts
-  get "users/:id/confirm" => "users#confirm"
+  get "users/:id/confirm" => "users#confirm", as: "confirm"
   resources :users, only: [:index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
