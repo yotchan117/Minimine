4ba0b2dea3192b0825d44e2e9dcb7c837d56601f
EC2 Default User <yotchan32323@gmail.com>
2022-12-13 07:41:01 +0000

    パスワード変更画面へのリンクを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update, :quit, :destroy]
-  before_action :ensure_guest_user, only: [:edit]
+  before_action :ensure_guest_user, only: [:edit, :quit]
 
   def index
     @users = User.order(created_at: :desc).page(params[:page]).per(20)
